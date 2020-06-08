5c2e94d9b18a533dba7463c94f3056cef9cad1ae
60058221 <jylee1164@gmail.com>
2020-06-08 15:48:45 +0900

    m5

--- Cat2/code/.ipynb_checkpoints/da_func-checkpoint.py
+++ Cat2/code/.ipynb_checkpoints/da_func-checkpoint.py
@@ -3,9 +3,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def countplot_cols(df, col_lists, ncols=3, dropna=True):
+def countplot_cols(df, col_lists, hue=None, ncols=3, dropna=True, annot=True):
     if dropna == False:
-        df = df.loc[:, col_lists].copy()
+        new_df = df.loc[:, col_lists].copy()
+        if hue is not None:
+            new_df[hue] = df[hue]
+        new_df = df
         df = df.fillna('NaN')
     nrows = len(col_lists) // ncols
     if len(col_lists) % ncols != 0:
@@ -13,21 +16,32 @@ def countplot_cols(df, col_lists, ncols=3, dropna=True):
 
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(7*ncols, 7*nrows))
     for i, c in enumerate(col_lists):
-        value_count = df[c].value_counts(dropna=dropna)
-        value_count.index = value_count.index.astype(str)
-        total = len(df[c])
-
-        ax = axes[i//ncols, i%ncols]
-        sns.countplot(x=c, data=df[[c]], ax=ax)
+        if nrows > 1:
+            ax = axes[i//ncols, i%ncols]
+        else:
+            ax = axes[i%ncols]
+        sns.countplot(x=c, hue=hue, data=df, ax=ax)
         
-        x_ticks = [x.get_text() for x in ax.get_xticklabels()]
-        for p, t in zip(ax.patches, x_ticks):            
-            height = p.get_height()
-            ax.text(p.get_x()+p.get_width()/2.,
-                    height,
-                    "%d (%.2f %%)" % (value_count[t], value_count[t]/total * 100),
-                    fontdict={'size':14},
-                    ha="center") 
+        if annot:
+            x_ticks = [x.get_text() for x in ax.get_xticklabels()]
+
+            value_count = df[c].value_counts(dropna=dropna)
+            value_count.index = value_count.index.astype(str)
+            total = len(df[c])
+            
+            for p, t in zip(ax.patches, x_ticks):            
+                height = p.get_height()
+                if hue is not None:
+                    #print(height)
+                    txt = "%d (%.2f %%) / %d (%.2f %%)" % (height, height/total * 100,
+                                                           value_count[t]-height, (value_count[t]-height)/total * 100)
+                else:
+                    txt = "%d (%.2f %%)" % (value_count[t], value_count[t]/total * 100)
+                ax.text(p.get_x()+p.get_width()/2.,
+                        height,
+                        txt,
+                        fontdict={'size':14},
+                        ha="center") 
 
         ax.title.set_text(c)
 
@@ -51,7 +65,10 @@ def countplot_cols_hc(df, col_lists, ncols=3, dropna=True):
         value_count.index = value_count.index.astype(str)
         total = len(df[c])
 
-        ax = axes[i//ncols, i%ncols]
+        if nrows > 1:
+            ax = axes[i//ncols, i%ncols]
+        else:
+            ax = axes[i%ncols]
         sns.countplot(y=c, data=df[[c]], ax=ax)
 
         ax.title.set_text(c)
@@ -59,3 +76,9 @@ def countplot_cols_hc(df, col_lists, ncols=3, dropna=True):
     
     plt.tight_layout()
     plt.show()
+    
+    
+def jaccard_similarity(list1, list2):
+    s1 = set(list1)
+    s2 = set(list2)
+    return len(s1.intersection(s2)) / len(s1.union(s2))
